765408c94b20f804341c45834f872decad2a963e
William Hicks <w.hicks917@gmail.com>
2017-07-07 19:37:48 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
+
+function increaseRankBy(n) {
+  console.clear();
+  var thing = document.querySelectorAll('.ranked-list');
+  for (let x = 0; x < thing.length; x++) {
+    let list = thing[x].children;
+    for (let i = 0; i < list.length; i++) {
+      let li = list[i];
+      li.innerHTML = parseInt(li.innerHTML) + n
+    }
+  }
+}
+
+function deepestChild() {
+  console.clear();
+  var node = document.getElementById('grand-node'),
+    parse = true,
+    count = 0;
+  while (parse) {
+    var previousNode = node,
+      node = node.children[0];
+    if (node != undefined) {
+      count += 1
+    }
+    else {
+      parse = false
+    }
+  }
+  return previousNode;
+}
